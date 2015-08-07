d343bf38c772fc10412a7b938bf717ed70692adc
Josh Ho <joshho@yahoo-inc.com>
2015-08-07 08:36:46 +0800

    project 2

---
+++ app/src/main/java/com/yahoo/joshhoy/gridimagesearch/EndlessScrollListener.java
@@ -0,0 +1,65 @@
+package com.yahoo.joshhoy.gridimagesearch;
+
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+
+public abstract class EndlessScrollListener implements OnScrollListener {
+    // The minimum # of items below your current scroll position before loading more.
+    private int visibleThreshold = 5;
+    private int currentPage = 0;
+    private int previousTotalItemCount = 0;
+    // true if waiting for data to load
+    private boolean loading = true;
+    private int startingPageIndex = 0;
+
+    public EndlessScrollListener() {
+    }
+
+    public EndlessScrollListener(int visibleThreshold) {
+        this.visibleThreshold = visibleThreshold;
+    }
+
+    public EndlessScrollListener(int visibleThreshold, int startPage) {
+        this.visibleThreshold = visibleThreshold;
+        this.startingPageIndex = startPage;
+        this.currentPage = startPage;
+    }
+
+    // This happens many times a second during a scroll, so be wary of the code you place here.
+    // We are given a few useful parameters to help us work out if we need to load some more data,
+    // but first we check if we are waiting for the previous load to finish.
+    @Override
+    public void onScroll(AbsListView view,int firstVisibleItem,int visibleItemCount,int totalItemCount)
+        {
+        // If the total item count is zero and the previous isn't, assume the list is invalid & reset it
+        if (totalItemCount < previousTotalItemCount) {
+            this.currentPage = this.startingPageIndex;
+            this.previousTotalItemCount = totalItemCount;
+            if (totalItemCount == 0) { this.loading = true; }
+        }
+
+        // if still loading also check count to decide loading is done
+        // then update loading flag, count & page
+        // number and total item count.
+        if (loading && (totalItemCount > previousTotalItemCount)) {
+            loading = false;
+            previousTotalItemCount = totalItemCount;
+            currentPage++;
+        }
+
+        // if not loading also check if threshold was crossed
+        // then load more data (incrementing page) and set loading flag true
+         if (!loading && (totalItemCount - visibleItemCount)<=(firstVisibleItem + visibleThreshold)) {
+            onLoadMore(currentPage + 1, totalItemCount);
+            loading = true;
+        }
+    }
+
+    // process to load more data based on page
+    public abstract void onLoadMore(int page, int totalItemsCount);
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+        // Don't take any action on changed
+    }
+}
---
+++ app/src/main/java/com/yahoo/joshhoy/gridimagesearch/Settings.java
@@ -0,0 +1,14 @@
+package com.yahoo.joshhoy.gridimagesearch;
+
+import java.io.Serializable;
+
+public class Settings implements Serializable {
+	public static final String kSETTINGS = "settings";
+
+	// HELP, I've fallen and can't get up
+	private static final long serialVersionUID = 1L;
+	public String siteFilter = "";
+	public String imageSize = "";
+	public String typeFilter = "";
+	public String colorFilter = "";
+}
